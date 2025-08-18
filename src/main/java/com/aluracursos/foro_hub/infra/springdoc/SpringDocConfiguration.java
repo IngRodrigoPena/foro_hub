dfa9d4fd78ddcff154651ea9664a438ba4946f59
IngRodrigoPena <ingrodrigopena@gmail.com>
2025-08-18 11:58:06 -0600

    B18 Documentacion con Swagger

--- src/main/java/com/aluracursos/foro_hub/infra/springdoc/SpringDocConfiguration.java
+++ src/main/java/com/aluracursos/foro_hub/infra/springdoc/SpringDocConfiguration.java
@@ -1,7 +1,9 @@
 package com.aluracursos.foro_hub.infra.springdoc;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,8 +24,16 @@ public class SpringDocConfiguration {
                 )
                 .info(new Info()
                         .title("API ForoHub")
-                        .description("Documentación de la API ForoHub con SpringDoc y JWT")
+                        .description("API Rest de la aplicación Foro Hub, que contiene:\n" +
+                                "- Funcionalidades CRUD de Tópicos\n" +
+                                "- Documentación con SpringDoc y JWT\n" +
+                                "\n⚠\uFE0F ESTADO: EN DESARROLLO")
                         .version("1.0.0")
-                );
+                        .contact(new Contact()
+                             .name("Equipo Backend")
+                             .email("backend@foro_hub.mx"))
+                        .license(new License()
+                             .name("Apache 2.0")
+                             .url("http://foro_hub.mx/api/licencia")));
     }
 }
